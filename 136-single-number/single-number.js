22ef575faf87a74c7e06950f12e4394418d5cc93
PrikshitRana98 <87004200+PrikshitRana98@users.noreply.github.com>
2024-06-11 17:18:46 +0530

    Time: 73 ms (25.83%) | Memory: 53.9 MB (23.45%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -2,20 +2,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-
- function getKeyByValue(object, value) {
-    return Object.keys(object).find(key =>
-        object[key] === value);
-}
+ 
+ 
 var singleNumber = function(nums) {
-    let count={}
-    for(let ele of nums){
-        if(count.hasOwnProperty(ele)){
-            count[ele]=count[ele]+1
-        }else{
-            count[ele]=1
-        }
-    }
-    return Object.keys(count).find(key=>count[key]==1)
-    
+    let newObj= nums.reduce((acc, num) => {
+        acc[num] = (acc[num] || 0) + 1;
+        return acc;
+    }, {});
+    return Object.keys(newObj).find(key=>newObj[key]==1)
 };
