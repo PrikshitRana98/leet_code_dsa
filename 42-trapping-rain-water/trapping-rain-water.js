6d5b5dc659c4965a1eca43c0711b0fe74a4275d2
PrikshitRana98 <87004200+PrikshitRana98@users.noreply.github.com>
2024-08-05 05:50:54 +0530

    Time: 49 ms (93.74%) | Memory: 51 MB (54.38%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -6,25 +6,25 @@ var trap = function(height) {
     if(height.length<=0){
         return 0
     }
+    let n=height.length
+    let l=0
+    let r=n-1
 
-    let left=new Array(height.length).fill(0);
-    let right=new Array(height.length).fill(0);
-    let l_max=0;
-    let r_max=0 
+    let l_max=height[l];
+    let r_max=height[r];
 
-    for(let i=0;i<=height.length-1;i++){
-        l_max=Math.max(height[i],l_max)
-        r_max=Math.max(height[height.length-1-i],r_max)
-        left[i]=l_max;
-        right[height.length-1-i]=r_max
-    }
-    
-    let res=0
-    
-    for(let i=0;i<=height.length-1;i++){
-        
-        res+=Math.min(left[i],right[i])-height[i]
-    }
-    
-    return res
+    let ans=0;
+
+    while(l<r){
+        if(height[l]<height[r]){
+            l_max=Math.max(l_max,height[l])
+            ans+=l_max-height[l]
+            l++;
+        }else{
+            r_max=Math.max(r_max,height[r])
+            ans+=r_max-height[r]
+            r--;
+        }
+    }    
+    return ans
 };
