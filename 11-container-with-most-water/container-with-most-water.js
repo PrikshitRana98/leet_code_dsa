97ed53914fc750ec1d21ae3e98f1aa48d68de388
PrikshitRana98 <87004200+PrikshitRana98@users.noreply.github.com>
2024-08-08 06:05:05 +0530

    Time: 1151 ms (5.03%) | Memory: 69.8 MB (5.04%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let res=0
+    let l=0; let r=height.length-1
+
+    while(l<=r){
+        let water=Math.min(height[l],height[r])*(r-l)
+        console.log(water)
+        if(water>res){
+            res=water
+        }
+        if(height[l]<height[r]){
+            l++
+        }else{
+            r--
+        }
+        
+    }
+    
+
+    return res
+    
+};
